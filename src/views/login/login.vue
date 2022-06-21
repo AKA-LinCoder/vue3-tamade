c6f73a7715188c01498f297a7504208faa9e4130
lsaac <2423860442@qq.com>
2022-06-22 00:41:26 +0800

    初始化项目

---
+++ src/views/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>main</div>
+  <div>MAind</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+const path = require('path')
+
+module.exports = {
+  //直接通过CLI提供给我们的选项来配置
+  outputDir: './build',
+  //配置方式2，和webpack属性完全一致，最后会进行合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       views: '@/views'
+  //     }
+  //   }
+  // }
+  //通过configureWebpack修改webpack的配置
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // },
+  //配置方式3
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('views', '@/views')
+  }
+}
