0e90149748946f20dd04cbaf373a5ff7fe23fd5d
AKA-LinCoder <2423860442@qq.com>
2022-07-18 19:54:47 +0800

    fix: 跨域

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
   //因为设置了映射解决跨域问题，所以使用api表示
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = ''
+  BASE_URL = 'http://152.136.185.210:5000'
 } else {
   BASE_URL = ''
 }
